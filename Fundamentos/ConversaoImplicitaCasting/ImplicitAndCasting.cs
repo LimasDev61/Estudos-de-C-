d620254a62c17410ab0f7a43185117416199b52f
BommerDev <renanalves000@gmail.com>
2025-07-20 01:28:31 -0300

    Conversão implicita e casting

---
+++ Fundamentos/ConversaoImplicitaCasting/ImplicitAndCasting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConversaoImplicitaCasting
+{
+    class ImplicitAndCasting
+    {
+        static void Main(string[] args)
+        {
+            // Conversão implicita menor < maior
+            Console.WriteLine("Conversão Implicita:");
+            float h1 = 35.2f;
+
+            double h2 = h1;
+
+            Console.WriteLine(h2.ToString("F2"));
+
+            // Erro da implicita quando utilizamos de maior > menor
+
+            double errorConversion = 35.20;
+
+            // erro nessa linha > float errorConversion1 = errorConversion; 
+
+            // Casting maior > menor;
+
+            Console.WriteLine();
+            Console.WriteLine("Casting:");
+            double h3 = 34.5;
+
+            int h4 = (int)h3; // Parte truncada, perde a parte decimal
+
+            Console.WriteLine(h4);
+            
+            // Casting para operação:
+            Console.WriteLine();
+            Console.WriteLine("Casting por operadores:");
+            int a = 5;
+            int b = 2;
+
+            double resultado = (double) a / b;
+            
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ Fundamentos/Operadores/AllOperators.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Operadores
+{
+    class AllOperators
+    {
+        static void Main(string[] args)
+        {
+            // Atribuição comum;
+            Console.WriteLine("Operadores Aritméticos:");
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+
+            Console.WriteLine("\nResultado sem parentêsis: " + n1 );
+            Console.WriteLine("Resultado com parentêsis:" + n2);
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------------");
+            // Operadores de Atribuição Cumulativa
+            Console.WriteLine();
+            Console.WriteLine("Operadores de Atribuição Cumulativa:");
+            int adicao = 10;
+            int subtracao = 10;
+            int multiplicacao = 10;
+            int divisao = 10;
+            int mod = 10;
+
+            adicao += 10;
+            subtracao -= 10;
+            multiplicacao *= 10;
+            divisao /= 2;
+            mod %= 3;
+
+            Console.WriteLine($"\nAdição: {adicao}");
+            Console.WriteLine($"Subtração: {subtracao}");
+            Console.WriteLine($"Multiplicação: {multiplicacao}");
+            Console.WriteLine($"Divisão: {divisao}");
+            Console.WriteLine($"Mod - Resto da divisão: {mod}");
+
+            // Operador de atribuição de adição também funciona com strings:
+            Console.WriteLine();
+            Console.WriteLine("Operador de Atribuição com String:");
+            string nome = "Hello ";
+            nome += "World!";
+
+            Console.WriteLine("\nTeste do operador de atribuição de adição, para strings: " + nome);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------------");
+
+            // Operadores aritméticos de atribuição
+            Console.WriteLine();
+            Console.WriteLine("Incremento e Decremento:");
+            Console.WriteLine("Use primeiro, incremente depois:");
+            int x = 10;
+            int y = x++;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.WriteLine("\nIncremente primeiro, use depois:");
+            int x1 = 10;
+            int y1 = ++x1;
+            Console.WriteLine(x1);
+            Console.WriteLine(y1);
+
+            Console.WriteLine();
+            Console.WriteLine("Use primeiro, decremente depois:");
+            int d1 = 10;
+            int d2 = d1--;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+            Console.WriteLine("\nDecremente primeiro, use depois:");
+            int d3 = 10;
+            int d4 = --d3;
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+        }
+    }
+}

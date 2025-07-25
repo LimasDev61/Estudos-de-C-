6610500a9b4c2a162cebb3525ba4b180521d4780
BommerDev <renanalves000@gmail.com>
2025-07-25 00:03:43 -0300

    Foi implementado ao diretorio estudos sobre operadores comparativos e lógicos

---
+++ Fundamentos/OperadoresComparativos/Comparatives.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OperadoresComparativos
+{
+    class Comparatives
+    {
+        static void Main(string[] args)
+        {
+            
+            // Comparativo maior
+            int a = 10;
+            bool condicao1 = a > 10;
+            Console.WriteLine("Condição 1, maior A > 10: " + condicao1);
+            
+            // Comparativo menor
+            int b = 10;
+            bool condicao2 = b < 11;
+            Console.WriteLine("Condição 2, menor B < 11: " + condicao2);
+            
+            // Comparativo maior ou igual
+            int c = 10;
+            bool condicao3 = c >= 10;
+            Console.WriteLine("Condição 3, maior ou igual C >= 10: " + condicao3);
+            
+            // Comparativo menor ou igual
+            int d = 11;
+            bool condicao4 = d <= 10;
+            Console.WriteLine("Condição 4, maior ou igual D <= 11: " + condicao4);
+            
+            // Comparativo igual
+            int e = 10;
+            int f = 9;
+            bool condicao5 = e == f;
+            Console.WriteLine("Condição 5, E igual a F: " + condicao5);
+            
+            // Comparativo diferente
+            int g = 10;
+            bool condicao6 = g != 11;
+            Console.WriteLine("Condição 6, G é diferente de 11: " + condicao6);
+        }
+    }
+}
+
---
+++ Fundamentos/OperadoresLogicos/Logic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Logic
+    {
+        static void Main(string[] args)
+        {
+            // Operador AND(&&)
+            bool condicao1 = 2 > 3 && 4 != 5;
+            Console.WriteLine("Condicao 1, operador AND, 2 > 3 && 4 != 5: " + condicao1);
+
+            // Operador OR(||)
+            bool condicao2 = 2 > 3 || 4 != 5;
+            Console.WriteLine("Condicao 2, operador OR, 2 > 3 || 4 != 5: " + condicao2);
+
+            // Teste de precedência 
+            bool condicao3 = 10 < 5;
+            bool condicao4 = condicao1 || condicao2 && condicao3;
+            Console.WriteLine("Teste de precedência, false || true && false = resultado, false: " + condicao4);
+
+            // Primeiro resolve condicao2 && condicao3 → true && false = false
+            // Depois resolve condicao1 || false → false || false = false
+
+            // Operador NOT (!)
+            bool condicao5 = !(4 == 4);
+            Console.WriteLine("Condicao 5, operador NOT, !(4 == 4): " + condicao5);
+
+            // Outro exemplo com NOT e uma variável
+            bool estaChovendo = false;
+            bool sairDeCasa = !estaChovendo;
+            Console.WriteLine("Condicao 6, operador NOT, !estaChovendo: " + sairDeCasa);
+        }
+    }
+}

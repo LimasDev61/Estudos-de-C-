4bb9401ce9fa9a5168a6586a8992918f5921b5e5
BommerDev <renanalves000@gmail.com>
2025-07-30 22:52:43 -0300

    Alterado o nome dos projetos para facilitar o mapeamento, entre FOR e While!

---
+++ ExerciciosEstruturaRepetitiva/Exercicio_2_Quadrant_While/Quadrant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio_2_Quadrant_While
+{
+    class Quadrant
+    {
+        static void Main(string[] args)
+        {
+            string[] confirm = Console.ReadLine().Split(' ');
+            int quadrantX = Convert.ToInt32(confirm[0]);
+            int quadrantY = Convert.ToInt32(confirm[1]);
+
+            while (quadrantX != 0 && quadrantY != 0)
+            {
+                if (quadrantX == quadrantY)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (quadrantX < 0 && quadrantY > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (quadrantX < 0 && quadrantY < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                }
+
+                confirm = Console.ReadLine().Split(' ');
+                quadrantX = Convert.ToInt32(confirm[0]);
+                quadrantY = Convert.ToInt32(confirm[1]);
+            }
+        }
+    }
+}
---
+++ ExerciciosEstruturaRepetitiva/Exercicio_3_Fuel_While/Fuel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercicio_3_Fuel_While
+{
+    class Fuel
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            bool numero = int.TryParse(Console.ReadLine(), out int order);
+            while (order != 4)
+            {
+                if (order == 1)
+                {
+                    alcool++;
+                }
+                else if (order == 2)
+                {
+                    gasolina++;
+                }
+                else if (order == 3)
+                {
+                    diesel++;
+                }
+
+                int.TryParse(Console.ReadLine(), out order);
+            }
+
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}

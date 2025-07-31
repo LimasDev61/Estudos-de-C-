9e703bff80d86a555e48bda5a8dbfa7b603443d8
BommerDev <renanalves000@gmail.com>
2025-07-30 23:27:53 -0300

    Estudo sobre a estrutura repetitiva FOR

---
+++ Fundamentos/RepetitivaFor/For.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RepetitivaFor
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0; 
+
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            bool numeros = int.TryParse(Console.ReadLine(), out int quantidade);
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.Write($"Valor #{i + 1}: ");
+                bool n = int.TryParse(Console.ReadLine(), out int acumulativo);
+                soma += acumulativo;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}

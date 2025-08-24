503cc1062d9d96cd6f3dbbcd6ecf431c366161d6
BommerDev <renanalves000@gmail.com>
2025-08-24 01:21:18 -0300

    Exércicio proposto de aluguel de quartos, concluído

--- ExerciciosVetores/Exercicio_1_RoomRental/Rooms.cs
+++ ExerciciosVetores/Exercicio_1_RoomRental/Rooms.cs
@@ -2,6 +2,16 @@
 
 class Rooms
 {
+    /*
+     * A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelos
+     * números 1 a 10. Quando um estudante deseja alugar um quarto, deve-se registrar o nome e e-mail deste estudante.
+     * Fazer um programa que inicie com todos os dez quartos vazios, e depois leia uma quantidade N representando o
+     * número de estudantes que vão alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos N
+     * estudantes.
+     * Para cada registro de aluguel, informar o nome e e-mail do estudante, bem como qual dos quartos ele escolheu
+     * (de 1 a 10). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de todas
+     * ocupações do pensionato, por ordem de quarto.
+     */
     static void Main(string[] args)
     {
         Pensionato meuPensionato = new Pensionato();
